a8a9f88589e4ad6a186f2fec523150efad08a664
pugazhmukilan <pugazhmukilanoffical2004@gmail.com>
2023-11-13 18:57:42 +0530

    quicksort update

--- Quicksort.java
+++ Quicksort.java
@@ -1,46 +1,42 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
 public class Quicksort {
-    public static int  partition (int arr[],int low,int high){
-        int pi=arr[high];
+    public static  int partition(int arr[], int low ,int high){
+        int pi = arr[high];
         int i=low-1;
-
-        for (int j=low;j<high;j++){
-            if(arr[j]<pi){
+        for(int j = low;j<high;j++){
+            if (arr[j]<pi){
                 i++;
-
-                //sawp
-                int temp=arr[i];
+                int temp = arr[i];
                 arr[i]=arr[j];
                 arr[j]=temp;
-
             }
         }
         i++;
-        int temp=arr[i];
-        arr[i]=pi;
+        int temp = arr[i];
+        arr[i]=arr[high];
         arr[high]=temp;
-
-        return i;
-
-    }
-    public static void quicksort(int arr[],int low,int high){
-        if (low<=high){
-            int pi =partition(arr,low,high);
-
-            quicksort(arr,low,pi-1);
-            quicksort(arr,pi+1,high);
-        }
-    }
-
-
-    public static void main(String[] args) {
         
-        int array[]={2,5,67,3,6,8,1};
-        int n=array.length;
-        quicksort(array,0,n-1);
-
-        for (int i=0;i<n;i++){
-            System.out.print(array[i]+" ");
-        }
+        return i;
     }
     
+    public static void quicksort(int arr[],int low,int high){
+        if (low<high){
+            int pindx= partition(arr, low , high);
+            
+            quicksort(arr,low,pindx-1);
+            quicksort(arr,pindx+1,high);
+        }
+    }
+    public static void main(String[] args) {
+       int array[]={2,1,9,10,3,8};
+       int n = array.length;
+       
+       quicksort(array,0,n-1);
+       
+       for(int i = 0;i<array.length;i++){
+           System.out.print(array[i]+" ");
+       }
+    }
 }
